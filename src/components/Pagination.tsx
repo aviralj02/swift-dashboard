eae7cf313c5ab9304091861c7ef206d2d397ab6f
aviralj02 <aviraljain1311@gmail.com>
2024-12-25 02:53:06 +0530

    refactor: :coffin: remove unused imports

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { ChangeEvent } from "react";
 
 type Props = {
